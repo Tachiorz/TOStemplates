a1589c9495d0f62e0d5d946f7ef48e101b64b2e1
Tachiorz <dytynenko.roman@gmail.com>
2015-12-13 16:18:08 +0200

    updates.

--- pale2bin.py
+++ pale2bin.py
@@ -2,6 +2,7 @@ __author__ = 'Tachi'
 import sys
 import binascii
 import struct
+import zlib
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
@@ -10,13 +11,32 @@ if __name__ == "__main__":
         with open(sys.argv[1][:-3] + "bin", 'wb') as fw:
             lines = f.readlines()
             for l in lines:
-                # Send@09:07:54.885 060001000000070000000035810DE1F9
-                time_str, packet = l.split(' ')
+                # Recv 11:17:13.325 BC_NORMAL 0	4F00FFFFFFFF210002000000958AF004010010010440AA30BCBD639041C0D286BC
+                _, time_str, packet_name, packet = l.split(' ')
+                size, packet = packet.split('\t')
                 packet_bin = binascii.unhexlify(packet.strip(' \r\n'))
+                if packet_name.upper() in ('ZC_NORMAL', 'BC_NORMAL'):
+                    if packet_bin[12:14] == "\x8D\xFA":
+                        size = struct.unpack("H", packet_bin[14:16])[0]
+                        sub_op = struct.unpack("I", packet_bin[8:12])[0]
+                        buf = "\x00\x00" + zlib.decompress(packet_bin[16:16+size], -zlib.MAX_WBITS)
+                        new_size = struct.pack("H", 12 + len(buf))  # fix xC_NORMAL size
+                        packet_bin = packet_bin[:6] + new_size + packet_bin[8:12] + buf
+                        print packet_name, sub_op, "compressed"
+                elif packet_name.upper() in ('ZC_ABILITY_LIST', 'ZC_SKILL_LIST'):
+                    if packet_bin[14:16] == "\x8D\xFA":
+                        size = struct.unpack("H", packet_bin[16:18])[0]
+                        buf = "\x00\x00" + zlib.decompress(packet_bin[18:18+size], -zlib.MAX_WBITS)
+                        packet_bin = packet_bin[:14] + buf
+                elif packet_name.upper() == 'ZC_ITEM_INVENTORY_LIST':
+                    if packet_bin[12:14] == "\x8D\xFA":
+                        size = struct.unpack("H", packet_bin[14:16])[0]
+                        buf = "\x00\x00" + zlib.decompress(packet_bin[16:16+size], -zlib.MAX_WBITS)
+                        packet_bin = packet_bin[:12] + buf
                 if l[0] == 'S':
                     direction = 0
                 else:
                     direction = 1
-                buf = struct.pack("12sHB", time_str[5:], len(packet_bin), direction)
+                buf = struct.pack("12sHB", time_str, len(packet_bin), direction)
                 fw.write(buf)
                 fw.write(packet_bin)
